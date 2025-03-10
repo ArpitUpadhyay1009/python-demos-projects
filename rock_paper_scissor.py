33094f396f6f924e948e00a19e022b3da3d08228
ArpitUpadhyay1009 <arpitupadhyay186@gmail.com>
2025-03-10 22:34:05 +0530

    made some changes

--- rock_paper_scissor.py
+++ rock_paper_scissor.py
@@ -21,18 +21,20 @@ scissor = '''    _______
       (____)
 ---.__(___)'''
 
-choice = int(input("Choose your option: 1 for rock, 2 for paper, 3 for scissors"))
-computer_choice = random.randint(1, 3)
+choice = int(input("Choose your option: 1 for rock, 2 for paper, 3 for scissors\n"))
+computer_choice = random.randint(0, 2)
 game = [rock, paper, scissor]
 print("Your choice:")
 print(game[choice-1])
 print("Computer's Choice:")
-print(game[computer_choice - 1])
-if ((choice == 1 and computer_choice == 2) or
-        (choice == 2 and computer_choice == 3) or
-        (choice == 3 and computer_choice == 1)):
+print(game[computer_choice])
+if choice > 3 or choice < 1:
+    print("Invalid choice you lose!")
+elif ((choice == 1 and computer_choice == 1) or
+        (choice == 2 and computer_choice == 2) or
+        (choice == 3 and computer_choice == 0)):
     print("You lose")
-elif choice == computer_choice:
+elif choice == computer_choice + 1:
     print("It's a tie")
 else:
     print("You win")
